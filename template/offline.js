a0946dd81d8a44ac16ff7315ea8e9fa2134def7d
Gyusun Yeom <omniavinco@gmail.com>
2017-06-26 18:09:27 +0900

    offline.js in es5

--- template/offline.js
+++ template/offline.js
@@ -1,30 +1,36 @@
-const CACHE_NAME = "offline-cache";
-const OFFLINE_URL = "{{=it.prefix}}offline.html";
+var CACHE_NAME = "offline-cache";
+var OFFLINE_URL = "{{=it.prefix}}offline.html";
 
-self.addEventListener(
-    "install",
-    event => {
+self.addEventListener("install", function(event) {
         var offlineRequest = new Request(OFFLINE_URL);
         event.waitUntil(
             fetch(offlineRequest).then(
-                response => caches.open(CACHE_NAME).then(
-                    cache => cache.put(offlineRequest, response)
-                )
+                function(response) {
+                    return caches.open(CACHE_NAME).then(
+                        function(cache) {
+                            return cache.put(offlineRequest, response);
+                        }
+                    );
+                }
             )
         );
     }
 );
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
     var request = event.request;
     if (request.method !== 'GET') {
         return;
     }
     event.respondWith(
         fetch(request).catch(
-            error => caches.open(CACHE_NAME).then(
-                cache => cache.match(OFFLINE_URL)
-            )
+            function(error) {
+                return caches.open(CACHE_NAME).then(
+                    function(cache) {
+                        return cache.match(OFFLINE_URL);
+                    }
+                );
+            }
         )
     );
 });
